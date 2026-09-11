508430160df5013ad2e2f7895e6195828df2d509
Raeshana <rsookhoo@andrew.cmu.edu>
2026-09-10 21:14:08 -0400

    Graveyard scene imported(?)

--- PlayMode.hpp
+++ PlayMode.hpp
@@ -28,22 +28,22 @@ struct PlayMode : Mode {
 	//local copy of the game scene (so code can change it during gameplay):
 	Scene scene;
 
-	//hexapod leg to wobble:
-	Scene::Transform *hip = nullptr;
-	Scene::Transform *upper_leg = nullptr;
-	Scene::Transform *lower_leg = nullptr;
-	glm::quat hip_base_rotation;
-	glm::quat upper_leg_base_rotation;
-	glm::quat lower_leg_base_rotation;
-	float wobble = 0.0f;
+	//graveyard leg to wobble:
+	// Scene::Transform *hip = nullptr;
+	// Scene::Transform *upper_leg = nullptr;
+	// Scene::Transform *lower_leg = nullptr;
+	// glm::quat hip_base_rotation;
+	// glm::quat upper_leg_base_rotation;
+	// glm::quat lower_leg_base_rotation;
+	// float wobble = 0.0f;
 
-	glm::vec3 get_leg_tip_position();
+	// glm::vec3 get_leg_tip_position();
 
-	//music coming from the tip of the leg (as a demonstration):
-	std::shared_ptr< Sound::PlayingSample > leg_tip_loop;
+	// //music coming from the tip of the leg (as a demonstration):
+	// std::shared_ptr< Sound::PlayingSample > leg_tip_loop;
 
-	//car honk sound:
-	std::shared_ptr< Sound::PlayingSample > honk_oneshot;
+	// //car honk sound:
+	// std::shared_ptr< Sound::PlayingSample > honk_oneshot;
 	
 	//camera:
 	Scene::Camera *camera = nullptr;
